35242046abdd862047b4ab3f653a432a2f19c8dd
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-08-03 14:49:37 +0200

    update

--- run.js
+++ run.js
@@ -1,13 +1,33 @@
 canvas = document.getElementById("gameWindow")
 ctx = canvas.getContext("2d")
 
+fps = 0
+timePassed = 0
+fpsData = []
+
+function countFps(framesPassed, lastFrame) {
+    date = new Date()
+    timeNow = date.getTime()
+    if (timeNow >= lastFrame + 1000) {
+        lastFrame = timeNow
+        document.getElementById("fpsLabel").innerHTML = "fps: " + framesPassed
+        framesPassed = 0
+    } else {
+        framesPassed = framesPassed + 1
+    }
+    return [framesPassed, lastFrame]
+}
+
 function clearWindow() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function gameUpdate() {
     clearWindow()
+    fpsData = countFps(fps, timePassed)
+    fps = fpsData[0]
+    timePassed = fpsData[1]
     console.log("update")
 }
 
-setInterval(gameUpdate, 300)
+setInterval(gameUpdate, 3)
