2cb60cde4cf442ec36a20b60dcf259dd757ca992
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-08-05 14:07:53 +0200

    1.0.1.1

--- objects.js
+++ objects.js
@@ -4,5 +4,11 @@ function addObject(objectType) {
         createMesh(cubeMatrix, 150 * amountOfObjects, 0, 0)
         amountOfObjects = amountOfObjects + 1
         document.getElementById("amountOfObjects").innerHTML = "amount of objects: " + amountOfObjects
+        objectHolder = document.getElementById("objectHolder")
+        button = document.createElement("button");
+        objectHolder.appendChild(button);
+        button.innerHTML = "cube"
+        button.id = "object"
+        button.style.top = (40 * (amountOfObjects - 1) + 20) + "px"
     }
 }
