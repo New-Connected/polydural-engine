6ab45c6c2eced4448ad66891715ffb2387480350
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-08-05 16:23:28 +0200

    1.0.1.4

--- objects.js
+++ objects.js
@@ -1,8 +1,11 @@
 amountOfObjects = 0
+objectSelected = [-1, "a"]
+
 function addObject(objectType) {
     if (objectType == "cube") {
         let nameNum = amountOfObjects + 1
         let name = "cube(" + nameNum + ")"
+        let buttonName = "object" + nameNum
         createMesh(cubeMatrix, 150 * amountOfObjects, 0, 0, name)
         amountOfObjects = amountOfObjects + 1
         document.getElementById("amountOfObjects").innerHTML = "amount of objects: " + amountOfObjects
@@ -10,18 +13,31 @@ function addObject(objectType) {
         button = document.createElement("button");
         objectHolder.appendChild(button);
         button.innerHTML = "cube (" + amountOfObjects + ")"
-        button.id = "object"
+        button.id = "object" + nameNum
+        button.className = "object"
         button.style.top = (40 * (amountOfObjects - 1) + 20) + "px"
         button.onclick = function () {
-            readObjectData(name)
+            readObjectData(name, buttonName)
         }
     }
 }
 
-function readObjectData(object) {
+function readObjectData(object, buttonName1) {
     for (x = 0; x < compiledMeshes.length; x++) {
         if (compiledMeshes[x][4] == object) {
-            console.log(compiledMeshes[x][4])
+            objectSelected = [x, buttonName1]
+            console.log(buttonName1)
+            console.log(object)
+            document.getElementById("objectName").innerHTML = "name: " + compiledMeshes[x][4]
+            document.getElementById("objectId").innerHTML = "id: " + x
         }
+    }
+}
+
+function deleteObject() {
+    if (objectSelected[0] >= 0) {
+        compiledMeshes.splice(objectSelected[0], 1)
+        document.getElementById(objectSelected[1]).remove()
+        objectSelected = [-1, "a"]
     }
 }
