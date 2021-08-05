b5b3d8af1c8e5d1140faf390c0fb9b3e3d859dec
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-08-05 12:51:41 +0200

    1.0.0.9

--- objects.js
+++ objects.js
@@ -3,5 +3,6 @@ function addObject(objectType) {
     if (objectType == "cube") {
         createMesh(cubeMatrix, 150 * amountOfObjects, 0, 0)
         amountOfObjects = amountOfObjects + 1
+        document.getElementById("amountOfObjects").innerHTML = "amount of objects: " + amountOfObjects
     }
 }
