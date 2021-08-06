a480d8f9390aef94295ed76f53d3e76ac34ffe51
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-08-06 16:01:58 +0200

    1.0.2.2

--- save.js
+++ save.js
@@ -1,9 +1,15 @@
 function saveScene() {
     window.localStorage.setItem("objects", JSON.stringify(compiledMeshes))
     window.localStorage.setItem("amountOfObjects", amountOfObjects)
+    window.localStorage.setItem("cameraPosX", camX)
+    window.localStorage.setItem("cameraPosY", camY)
+    window.localStorage.setItem("cameraPosZ", camZ)
 }
 function loadScene() {
     compiledMeshes = JSON.parse(window.localStorage.getItem("objects"))
     amountOfObjects = Number(window.localStorage.getItem("amountOfObjects"))
+    camX = Number(window.localStorage.getItem("cameraPosX"))
+    camY = Number(window.localStorage.getItem("cameraPosY"))
+    camZ = Number(window.localStorage.getItem("cameraPosZ"))
     reloadObjects()
 }
